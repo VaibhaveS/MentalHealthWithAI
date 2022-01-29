c8a23f53f1f8b7898c2f036ff5d3bbc7261ae127
kishan <kishansara22@gmail.com>
2022-01-29 17:26:28 +0530

    added some basic files

--- proj/routes.py
+++ proj/routes.py
@@ -1,8 +1,8 @@
 from proj import app
 from flask import render_template,url_for,redirect,flash,request
-# import speech_recognition as sr
-# from flask import jsonify
-# from flask import json
+import speech_recognition as sr
+from flask import jsonify
+from flask import json
 # import pickle
 # import numpy as np
 # import sklearn
@@ -11,5 +11,17 @@ from flask import render_template,url_for,redirect,flash,request
 # from sklearn import preprocessing
 
 @app.route('/')
-def hello():
-   return "Hello"
+@app.route('/home')
+def home():
+   return render_template('home.html',title='Home') 
+
+
+
+@app.route('/register',methods=['POST','GET'])
+def register():
+
+   return render_template('register.html',title='register')  
+
+@app.route('/login',methods=['POST','GET'])
+def login():
+   return render_template('login.html',title='login') 
