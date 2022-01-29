83a3958c0df1be166d3fdfc1c83420fecf519b13
kishan <kishansara22@gmail.com>
2022-01-29 18:03:54 +0530

    Revert "added some basic files"
    This reverts commit c8a23f53f1f8b7898c2f036ff5d3bbc7261ae127.

--- proj/routes.py
+++ proj/routes.py
@@ -1,8 +1,8 @@
 from proj import app
 from flask import render_template,url_for,redirect,flash,request
-import speech_recognition as sr
-from flask import jsonify
-from flask import json
+# import speech_recognition as sr
+# from flask import jsonify
+# from flask import json
 # import pickle
 # import numpy as np
 # import sklearn
@@ -11,17 +11,5 @@ from flask import json
 # from sklearn import preprocessing
 
 @app.route('/')
-@app.route('/home')
-def home():
-   return render_template('home.html',title='Home') 
-
-
-
-@app.route('/register',methods=['POST','GET'])
-def register():
-
-   return render_template('register.html',title='register')  
-
-@app.route('/login',methods=['POST','GET'])
-def login():
-   return render_template('login.html',title='login') 
+def hello():
+   return "Hello"
