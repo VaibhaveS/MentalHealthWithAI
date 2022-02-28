0d837df04c4060dbc81762fdb153ec2fd719683f
balaji-m13 <mbalaji131224@gmail.com>
2022-02-28 10:37:48 +0530

    form data fetched in backend

--- proj/routes.py
+++ proj/routes.py
@@ -130,3 +130,11 @@ def cluster():
     grp3 = clusters[-1]
     print(grp3)
     return render_template('cluster.html',title='cluster',grp1 = grp1, grp2 = grp2, grp3 = grp3)
+
+def predictions():
+    form_response = {}
+    for i in range(1, 11, 1):
+        ques = "q" + str(i)
+        qi = request.form.get(ques)
+        form_response[ques] = qi
+    return form_response
