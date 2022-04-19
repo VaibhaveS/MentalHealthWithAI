feea174785416699132b2d2a3b160820c871d247
kishan <kishansara22@gmail.com>
2022-04-19 17:54:30 +0530

    close to final copy updated

--- proj/templates/js/main.js
+++ proj/templates/js/main.js
@@ -192,12 +192,10 @@ function myCallBack(xhr){
         ele2.innerHTML=word.slice(1,word.length-2);
         let br1 = document.createElement("br");
         ele2.appendChild(br1)
-        ele2.appendChild(document.createElement("br"))
-        ele2.appendChild(document.createElement("br"))
         let btn = document.createElement("a");
         var spa = document.createElement('span');
         btn.appendChild(spa);
-        spa.innerHTML = "VIEW";
+        spa.innerHTML = "Results";
         btn.type = "submit";
         btn.name = "formBtn";
         btn.href="http://127.0.0.1:5000/analysis"
